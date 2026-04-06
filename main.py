b40418af7f76766a3115a19010c51ba6307e2a71
Nhut <lecongminhnhut6533@gmail.com>
2026-04-06 21:24:00 +0700

    Thêm video demo

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import uvicorn
 app = FastAPI()
 
 # Dán link Pinggy từ Colab vào đây Ollama-Pinggy.ipynb]
-OLLAMA_URL = "https://efxyq-34-148-42-172.run.pinggy-free.link/api/generate"
+OLLAMA_URL = "https://vgnlb-136-110-15-240.run.pinggy-free.link/api/generate"
 
 class EmailRequest(BaseModel):
     text: str
